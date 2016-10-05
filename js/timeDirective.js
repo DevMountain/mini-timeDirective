f9d05be39c61aecbfc1b4f99c5d20fe350ed9adc
Thai Tran <Thai.Tran5656@gmail.com>
2016-10-04 19:40:57 -0700

    completed

--- js/timeDirective.js
+++ js/timeDirective.js
@@ -3,7 +3,7 @@ angular.module('timeApp').directive('showTime',function(){
       restrict:'E',
       template:'<div> The current time is {{time}} </div>',
       link: function(scope, element, attrs) {
-        var currentTime= new Date();
+        currentTime= new Date();
         scope.time= currentTime.toString();
       }
     }
